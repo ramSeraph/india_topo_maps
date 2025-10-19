e9e898a403e7bbf690a438c31962ad93f046e821
ramSeraph <kandimalla.sreeram@gmail.com>
2025-10-19 17:19:31 +0530

    bugfix in compress workflow

--- 50k/osm/util/get_work_for_compressor.sh
+++ 50k/osm/util/get_work_for_compressor.sh
@@ -3,7 +3,7 @@
 gh release download 50k-osm-orig -p listing_files.csv -O pdfs_listing.csv --clobber
 gh release download 50k-osm-jpg -p listing_files.csv -O jpgs_listing.csv --clobber
 
-comm -23 <(cat pdfs_listing.csv | cut -d"," -f1 | sort) <(cat jpgs_listing.csv | cut -d"," -f1 | sort) | cut -f1 | sed '/^[[:space:]]*$/d' > temp.txt
+comm -23 <(cat pdfs_listing.csv | cut -d"," -f1 | cut -d"." -f1 | sort) <(cat jpgs_listing.csv | cut -d"," -f1 | cut -d"." -f1 | sort) | cut -f1 | sed '/^[[:space:]]*$/d' | sed 's/$/.pdf/' > temp.txt
 
 comm -23 <(cat temp.txt | sort) <(cat bad_files.txt | sort) | cut -f1 | sed '/^[[:space:]]*$/d' > $1
 
